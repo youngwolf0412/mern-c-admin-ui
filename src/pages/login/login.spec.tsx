a5ea77e8ee871a75925e844a4fb51bdb99f7f1e9
Brijesh Yadav <kumar.brijesh0412@gmail.com>
2025-04-14 16:53:42 +0530

    first component test

---
+++ src/pages/login/login.spec.tsx
@@ -0,0 +1,20 @@
+import { describe, expect, it } from "vitest";
+import { render, screen } from "@testing-library/react";
+import LoginPage from "./Login";
+
+describe("login page", () => {
+  it("should render with required fields", () => {
+    render(<LoginPage />);
+    // getBy ->throws an error if the element is not found
+    // queryBy -> returns null if the element is not found
+    // findBy -> returns a promise that resolves when the element is found
+    expect(screen.getByText("Sign in")).toBeInTheDocument();
+    expect(screen.getByPlaceholderText("Username")).toBeInTheDocument();
+    expect(screen.getByPlaceholderText("Password")).toBeInTheDocument();
+    expect(screen.getByRole("button", { name: "Log in" })).toBeInTheDocument();
+    expect(
+      screen.getByRole("checkbox", { name: "Remember me" })
+    ).toBeInTheDocument();
+    expect(screen.getByText("Forgot password")).toBeInTheDocument();
+  });
+});
--- src/router.tsx
+++ src/router.tsx
@@ -1,8 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import Categories from "./pages/Categories";
+import LoginPage from "./pages/login/login";
 
 export const router = createBrowserRouter([
   { path: "/", element: <HomePage /> },
-  { path: "/categories", element: <Categories /> },
+  { path: "/auth/login", element: <LoginPage /> },
 ]);
