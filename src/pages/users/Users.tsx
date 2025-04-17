85a0ad787ad609bd219282186cb0eb85f0f6ca3a
Brijesh Yadav <kumar.brijesh0412@gmail.com>
2025-04-17 08:43:12 +0530

    feat: implement user fetching functionality and enhance Users page with loading state

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,11 +1,20 @@
 import { Breadcrumb } from "antd";
 import { RightOutlined } from "@ant-design/icons";
 import { Link } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import { getUsers } from "../../http/api";
+import { User } from "../../types";
 
 const Users = () => {
+  const { data: users, isLoading } = useQuery({
+    queryKey: ["users"],
+    queryFn: async () => {
+      const res = await getUsers();
+      return res.data;
+    },
+  });
   return (
-    <div>
-      {" "}
+    <>
       <Breadcrumb
         separator={<RightOutlined />}
         items={[
@@ -17,7 +26,17 @@ const Users = () => {
           },
         ]}
       />
-    </div>
+      {isLoading && <div>Loading...</div>}
+
+      {users && (
+        <ul>
+          <h1>Users</h1>
+          {users["data"].map((user: User) => (
+            <li key={user.id}>{user.firstName}</li>
+          ))}
+        </ul>
+      )}
+    </>
   );
 };
 
