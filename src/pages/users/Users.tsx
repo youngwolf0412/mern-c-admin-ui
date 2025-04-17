961f39354df3fbd9bf07ef46c3d1392b50d20b0b
Brijesh Yadav <kumar.brijesh0412@gmail.com>
2025-04-17 10:29:22 +0530

    feat: add Add User button and drawer for user creation in Users page

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,10 +1,11 @@
-import { Breadcrumb, Table } from "antd";
-import { RightOutlined } from "@ant-design/icons";
+import { Breadcrumb, Button, Drawer, Space, Table } from "antd";
+import { PlusOutlined, RightOutlined } from "@ant-design/icons";
 import { Link } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import { getUsers } from "../../http/api";
 import { User } from "../../types";
 import UsersFilter from "./UsersFilter";
+import { useState } from "react";
 
 const columns = [
   {
@@ -30,6 +31,8 @@ const columns = [
 ];
 
 const Users = () => {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
   const { data: users, isLoading } = useQuery({
     queryKey: ["users"],
     queryFn: async () => {
@@ -52,9 +55,52 @@ const Users = () => {
       />
       {isLoading && <div>Loading...</div>}
 
-      <UsersFilter />
+      <UsersFilter>
+        <Button
+          type="primary"
+          icon={<PlusOutlined />}
+          onClick={() => setDrawerOpen(true)}
+        >
+          Add User
+        </Button>
+      </UsersFilter>
 
-      <Table columns={columns} dataSource={users["data"]} />
+      <Table columns={columns} dataSource={users?.data} />
+
+      <Drawer
+        title="Create User"
+        width={720}
+        // styles={{ body: { backgroundColor: colorBgLayout } }}
+        destroyOnClose={true}
+        open={drawerOpen}
+        onClose={() => {
+          console.log("CLODINSG");
+
+          // form.resetFields();
+          // setCurrentEditingUser(null);
+          setDrawerOpen(false);
+        }}
+        extra={
+          <Space>
+            <Button
+              onClick={() => {
+                // form.resetFields();
+                // setDrawerOpen(false);
+              }}
+            >
+              Cancel
+            </Button>
+            <Button type="primary">Submit</Button>
+          </Space>
+        }
+      >
+        <p>some contetn....</p>
+        <p>some contetn....</p>
+        <p>some contetn....</p>
+        {/* <Form layout="vertical" form={form}>
+          <UserForm isEditMode={!!currentEditingUser} />
+        </Form> */}
+      </Drawer>
 
       {/* {users && (
         <ul>
